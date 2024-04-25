c2ceb29e43404988568f33a87739a575b4672153
Chin Timothy <Timothy.Chin@swisscom.com>
2024-04-25 19:59:12 +0200

    Added the diagram from data_statistics.py

--- data_statistics.py
+++ data_statistics.py
@@ -9,16 +9,16 @@ abalone_targets = pandas.read_excel('./Data/abalone_targets.xlsx')
 statistics_attributes = abalone_attributes.describe()
 statistics_targets = abalone_targets.describe()
 
-# To Check
+# To display the statistics
 print(statistics_attributes)
 print(statistics_targets)
 
-# Creating a Diagramm
+# Creating the "Distribution of Rings" Histogramm
 ring_counts = abalone_targets['Rings'].value_counts(normalize=True) * 100
-
 plt.figure(figsize=(10, 6))
 plt.bar(ring_counts.index, ring_counts.values, color='b', alpha=0.7)
 plt.xlabel('Number of Rings')
 plt.ylabel('Percentage (%)')
 plt.title('Distribution of Rings in Abalone Dataset')
 plt.show()
+
